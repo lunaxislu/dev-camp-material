34d8dd712d3e7839a844e99c7d1b43086453b608
최문길 <cmk0905@naver.com>
2024-03-06 03:56:59 +0900

    🎉 react-hook-form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,70 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import clsx from "clsx";
-
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 const inter = Inter({ subsets: ["latin"] });
+//  twmerge 를 이해하기 위한 예시 코드
+// function MyGenericInput(props) {
+//   const className = twMerge(`text-red-400 text-blue-400`);
+//   return (
+//     <button {...props} className={className}>
+//       버튼
+//     </button>
+//   );
+// }
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
+const onSubmit = (value: z.infer<typeof formSchema>) => {
+  console.log(value);
+};
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+  console.log(form.getFieldState().invalid);
   return (
     <main className={` ${inter.className}`}>
-      <Button type="submit" variant={"destructive"}>
-        버튼
-      </Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      )
     </main>
   );
 }
