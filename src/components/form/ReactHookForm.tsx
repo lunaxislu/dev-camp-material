5bac04f13b6aba951936f0d20786caad4c332c86
lunaxislu <cmk0905@naver.com>
2024-03-07 02:30:33 +0900

    ✅ zod를 활용한 react-hook-form

--- src/components/form/ReactHookForm.tsx
+++ src/components/form/ReactHookForm.tsx
@@ -1,54 +1,48 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { register } from "module";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
-interface FormFields {
-  email: string;
-  password: string;
-}
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type FormFields = z.infer<typeof schema>;
+
 const ReactHookForm = () => {
-  const form = useForm<FormFields>();
+  const form = useForm<FormFields>({
+    resolver: zodResolver(schema),
+  });
+
   const onSubmit: SubmitHandler<FormFields> = (data) => {
     console.log(data);
+    const { email, password } = data;
   };
-  console.log(form.register);
+
   return (
     <form
       className="flex flex-col p-10 gap-2"
       onSubmit={form.handleSubmit(onSubmit)}
     >
       <input
-        {...form.register("email", {
-          required: "email is required",
-          //     pattern: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
-          validate: (value) => {
-            if (!value.includes("@")) {
-              return "Please into The @";
-            }
-            return true; // validate는
-          },
-        })}
+        {...form.register("email")}
         className="border-solid border-black border-2 pl-4"
         type="text"
         placeholder="Email@naver.com"
       />
       {form.formState.errors.email && (
-        <div>{`${form.formState.errors.email.message} ;;`}</div>
+        <div>{`${form.formState.errors.email.message}`}</div>
       )}
       <input
-        {...form.register("password", {
-          required: true,
-          minLength: {
-            value: 8,
-            message: "must Have at least 8",
-          },
-        })}
+        {...form.register("password")}
         className="border-solid border-black border-2 pl-4"
         type="text"
         placeholder="lte 8"
       />
       {form.formState.errors.password && (
-        <div>{`${form.formState.errors.password.message} ;;`}</div>
+        <div>{`${form.formState.errors.password.message}`}</div>
       )}
       <button className="border-solid border-black py-4">submit</button>
     </form>
